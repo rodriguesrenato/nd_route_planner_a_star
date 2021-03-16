ca0aa3af6caad6ec86590a7f6c65b881f97518f9
rodriguesrenato <renato.rodrigues.rfr@gmail.com>
2021-03-16 15:11:36 -0300

    fix: AStartSearch function fixed

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@
 #include "route_model.h"
 #include "render.h"
 #include "route_planner.h"
+#include <ios>
+#include <limits>
 
 using namespace std::experimental;
 
@@ -55,23 +57,47 @@ int main(int argc, const char **argv)
             osm_data = std::move(*data);
     }
 
-    // Declare user coordinates variables
-    float start_x = 0.0;
-    float start_y = 0.0;
-    float end_x = 0.0;
-    float end_y = 0.0;
+    // Declare start and goal variables
+    float start_x, start_y, end_x, end_y;
 
-    // Get coordinates from User.
-    std::cout << "\nChoose a start coordinate (values between 0 and 100).\n  start_x:";
+    // Limit variables between 0 and 100
+    std::cout << "Enter a start_x from 0 to 100: ";
     std::cin >> start_x;
-    std::cout << "  start_y:";
+    while (!(std::cin) || start_x < 0 || start_x > 100)
+    {
+        std::cout << "Invalid entry. Enter a start_x from 0 to 100: ";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cin >> start_x;
+    }
+    std::cout << "Enter a start_y from 0 to 100: ";
     std::cin >> start_y;
-
-    std::cout << "\nChoose an end coordinate (values between 0 and 100).\n  end_x:";
+    while (!(std::cin) || start_y < 0 || start_y > 100)
+    {
+        std::cout << "Invalid entry. Enter a start_y from 0 to 100: ";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cin >> start_y;
+    }
+    std::cout << "Enter a end_x from 0 to 100: ";
     std::cin >> end_x;
-    std::cout << "  end_y:";
+    while (!(std::cin) || end_x < 0 || end_x > 100)
+    {
+        std::cout << "Invalid entry. Enter a end_x from 0 to 100: ";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cin >> end_x;
+    }
+    std::cout << "Enter a end_y from 0 to 100: ";
     std::cin >> end_y;
-    
+    while (!(std::cin) || end_y < 0 || end_y > 100)
+    {
+        std::cout << "Invalid entry. Enter a end_y from 0 to 100: ";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cin >> end_y;
+    }
+
     // Build Model.
     RouteModel model{osm_data};
 
--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -1,6 +1,7 @@
 #include "route_planner.h"
 #include <algorithm>
-
+using std::cout;
+// Iinitialize Route Planner and finde the closest node from the user start and end coordinates.
 RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y) : m_Model(model)
 {
     // Convert inputs to percentage:
@@ -11,35 +12,48 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
 
     start_node = &m_Model.FindClosestNode(start_x, start_y);
     end_node = &m_Model.FindClosestNode(end_x, end_y);
+    start_node->g_value = 0;
+    start_node->h_value = CalculateHValue(start_node);
+    end_node->h_value = 0;
+
+    std::cout << "start_node: " << start_node->x << "," << start_node->y << "\t| user start: " << start_x << "," << start_y << std::endl;
+    std::cout << "end_node: " << end_node->x << "," << end_node->y << "\t| user end: " << end_x << "," << end_y << std::endl;
 }
 
-float RoutePlanner::CalculateHValue(RouteModel::Node const *node)
+// Calculate the distance from the given node to the end_node
+float RoutePlanner::CalculateHValue(const RouteModel::Node *node)
 {
     return node->distance(*end_node);
 }
 
+// Find the neighbors of given node and populate it's neighbors list.
+// Iterate over each neigbour: set current node as parent,
+// calculate g and h, mark as visited and at last, added it in open_list.
 void RoutePlanner::AddNeighbors(RouteModel::Node *current_node)
 {
+    cout << "\ncurrent_node: " << current_node->x << "," << current_node->y << std::endl;
     current_node->FindNeighbors();
     for (RouteModel::Node *n : current_node->neighbors)
     {
-        if (!n->visited)
-        {
+        // cout << "    neighbor_node: " << n->x << "," << n->y << std::endl;
+        // if (!n->visited)
+        // {
             n->parent = current_node;
             n->h_value = CalculateHValue(n);
             n->g_value += current_node->g_value + current_node->distance(*n);
             n->visited = true;
             open_list.push_back(n);
-        }
-        //what if the sencodary path is shorter than the one already visited?? TODOTODO
+        // }
     }
 }
 
+// A comparison functor for the NextNode() sort function
 bool Compare(const RouteModel::Node *n1, const RouteModel::Node *n2)
 {
     return (n1->g_value + n1->h_value) > (n2->g_value + n2->h_value);
 }
 
+// Sort the open_list, get the node of lowest g+h value, remove it from open_list and return it.
 RouteModel::Node *RoutePlanner::NextNode()
 {
     std::sort(open_list.begin(), open_list.end(), Compare);
@@ -72,14 +86,13 @@ std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node 
     return path_found;
 }
 
-
 void RoutePlanner::AStarSearch()
 {
     RouteModel::Node *current_node = start_node;
     start_node->visited = true;
+    open_list.push_back(current_node);
     do
     {
-        AddNeighbors(current_node);
         current_node = NextNode();
         if (current_node == end_node)
         {
@@ -87,6 +100,7 @@ void RoutePlanner::AStarSearch()
             std::cout << "\nPath found!" << std::endl;
             return;
         }
+        AddNeighbors(current_node);
     } while (open_list.size() > 0);
 
     std::cout << "\nCan't find a path!" << std::endl;
