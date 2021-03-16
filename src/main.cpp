de3d65744945721b07cec3b4a6cb80ef62f5023a
rodriguesrenato <renato.rodrigues.rfr@gmail.com>
2021-03-16 15:35:48 -0300

    feat: Added user input helper function

--- src/main.cpp
+++ src/main.cpp
@@ -29,6 +29,22 @@ static std::optional<std::vector<std::byte>> ReadFile(const std::string &path)
     return std::move(contents);
 }
 
+//Helper function to get user input values validated in type and between 0 to 100 limits
+float user_input(std::string input_var){
+    float input_val = 0;
+    std::cout << "Enter a " << input_var << " from 0 to 100: ";
+    std::cin >> input_val;
+    while (!(std::cin) || input_val < 0 || input_val > 100)
+    {
+        std::cout << "Invalid entry. Enter a " << input_var << " from 0 to 100: ";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cin >> input_val;
+    }
+    return input_val;
+}
+
+
 int main(int argc, const char **argv)
 {
     std::string osm_data_file = "";
@@ -60,43 +76,52 @@ int main(int argc, const char **argv)
     // Declare start and goal variables
     float start_x, start_y, end_x, end_y;
 
+    // Get user inputs for each variable
+    start_x = user_input("start_x");
+    start_y = user_input("start_y");
+    end_x = user_input("end_x");
+    end_y = user_input("end_y");
+
     // Limit variables between 0 and 100
-    std::cout << "Enter a start_x from 0 to 100: ";
-    std::cin >> start_x;
-    while (!(std::cin) || start_x < 0 || start_x > 100)
-    {
-        std::cout << "Invalid entry. Enter a start_x from 0 to 100: ";
-        std::cin.clear();
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        std::cin >> start_x;
-    }
-    std::cout << "Enter a start_y from 0 to 100: ";
-    std::cin >> start_y;
-    while (!(std::cin) || start_y < 0 || start_y > 100)
-    {
-        std::cout << "Invalid entry. Enter a start_y from 0 to 100: ";
-        std::cin.clear();
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        std::cin >> start_y;
-    }
-    std::cout << "Enter a end_x from 0 to 100: ";
-    std::cin >> end_x;
-    while (!(std::cin) || end_x < 0 || end_x > 100)
-    {
-        std::cout << "Invalid entry. Enter a end_x from 0 to 100: ";
-        std::cin.clear();
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        std::cin >> end_x;
-    }
-    std::cout << "Enter a end_y from 0 to 100: ";
-    std::cin >> end_y;
-    while (!(std::cin) || end_y < 0 || end_y > 100)
-    {
-        std::cout << "Invalid entry. Enter a end_y from 0 to 100: ";
-        std::cin.clear();
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        std::cin >> end_y;
-    }
+    // std::cout << "Enter a start_x from 0 to 100: ";
+    // std::cin >> start_x;
+    // while (!(std::cin) || start_x < 0 || start_x > 100)
+    // {
+    //     std::cout << "Invalid entry. Enter a start_x from 0 to 100: ";
+    //     std::cin.clear();
+    //     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    //     std::cin >> start_x;
+    // }
+
+    // std::cout << "Enter a start_y from 0 to 100: ";
+    // std::cin >> start_y;
+    // while (!(std::cin) || start_y < 0 || start_y > 100)
+    // {
+    //     std::cout << "Invalid entry. Enter a start_y from 0 to 100: ";
+    //     std::cin.clear();
+    //     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    //     std::cin >> start_y;
+    // }
+
+    // std::cout << "Enter a end_x from 0 to 100: ";
+    // std::cin >> end_x;
+    // while (!(std::cin) || end_x < 0 || end_x > 100)
+    // {
+    //     std::cout << "Invalid entry. Enter a end_x from 0 to 100: ";
+    //     std::cin.clear();
+    //     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    //     std::cin >> end_x;
+    // }
+
+    // std::cout << "Enter a end_y from 0 to 100: ";
+    // std::cin >> end_y;
+    // while (!(std::cin) || end_y < 0 || end_y > 100)
+    // {
+    //     std::cout << "Invalid entry. Enter a end_y from 0 to 100: ";
+    //     std::cin.clear();
+    //     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    //     std::cin >> end_y;
+    // }
 
     // Build Model.
     RouteModel model{osm_data};
